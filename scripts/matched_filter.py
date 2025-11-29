c5cec7cec465509d0bec7bde9a91be3659726311
Rom0219 <crobinson1902@gmail.com>
2025-11-28 20:21:52 -0500

    Update matched_filter.py

--- scripts/matched_filter.py
+++ scripts/matched_filter.py
@@ -1,20 +1,13 @@
-# matched_filter.py
 import numpy as np
-from scipy.signal import fftconvolve
-from gwpy.signal import filter_design
+from pycbc.filter import matched_filter
+from pycbc.types import TimeSeries
 
-def whiten(strain, psd, fs):
-    # simple whitening: divide by sqrt(psd) in freq domain
-    # but better use gwpy.timeseries.whiten() or pycbc.filter.whiten
-    from gwpy.timeseries import TimeSeries
-    ts = TimeSeries(strain, sample_rate=fs)
-    return ts.whiten(window='hann', method='median', beta=4).value
+def compute_snr(strain, template, sample_rate):
 
-def matched_filter_whitened(data, template):
-    # data and template must be whitened and same sampling
-    corr = fftconvolve(data, template[::-1], mode='same')
-    # estimate SNR time series: corr / sqrt(<template,template>)
-    norm = np.sqrt(np.sum(template**2))
-    snr = corr / norm
-    return snr
+    data = TimeSeries(strain, delta_t=1/sample_rate)
+    temp = TimeSeries(template, delta_t=1/sample_rate)
 
+    snr = matched_filter(temp, data)
+    peak_snr = max(abs(snr))
+
+    return peak_snr
