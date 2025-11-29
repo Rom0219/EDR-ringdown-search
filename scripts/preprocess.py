ed4fdf3c80cbd00d6d54ab84c55e82797a0d223a
Rom0219 <crobinson1902@gmail.com>
2025-11-28 20:21:30 -0500

    Update preprocess.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,1 +1,12 @@
+from gwpy.timeseries import TimeSeries
+import numpy as np
+
+def preprocess(filename, f_low=20):
+    ts = TimeSeries.read(filename)
+
+    ts = ts.detrend()
+    ts = ts.highpass(f_low)
+    white = ts.whiten()
+
+    return white
 
